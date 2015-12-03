3e73e8e26d643b627c2b9221757cf81a642954bf
WeDemBoyz <kmagali120@gmail.com>
2015-12-04 00:25:25 +0100

    Added Product domain class

---
+++ grails-app/domain/justdevit/Product.groovy
@@ -0,0 +1,13 @@
+package justdevit
+
+class Product {
+    String id
+    String name
+    String mainImageUrl
+    double  salePrice
+    String sellerId
+    String sellerName
+
+    static constraints = {
+    }
+}
